f6a55634ae6e89d49eefa30d48d8dc3bf5226da3
Cascao <matheusfranco23@hotmail.com>
2023-08-15 00:05:47 -0300

    fix: enabling cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from "cors"
 import todoRoute from './src/routes/Todo.js';
 const app = express();
 const port = process.env.port || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/todo/', todoRoute);
 
 app.listen(port, () => {
